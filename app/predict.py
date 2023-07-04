9b34c1d5e33634cfcba4975b85820307a701da39
NasirXpython <Nasir.Iqbal.Dev@gmail.com>
2023-07-04 15:30:00 +0400

    Improve image processing utilities and validation

--- app/predict.py
+++ app/predict.py
@@ -2,18 +2,22 @@ from ultralytics import YOLO
 from .utils import read_image
 import numpy as np
 
-# Model version
-MODEL_VERSION = "v1"
-MODEL_PATH = f"./models/{MODEL_VERSION}/helmet_detector.pt"
+# Model cache to avoid reloading
+model_cache = {}
 
-# Load YOLOv8 model
-model = YOLO(MODEL_PATH)
+def get_model(model_version: str):
+    """Load and cache YOLO model"""
+    if model_version not in model_cache:
+        model_path = f"./models/{model_version}/helmet_detector.pt"
+        model_cache[model_version] = YOLO(model_path)
+    return model_cache[model_version]
 
-def predict(image: np.ndarray, conf: float = 0.5):
+def predict(image: np.ndarray, conf: float = 0.5, model_version: str = "v1"):
     """
     Run YOLOv8 prediction on an image
     Returns annotated image, bounding boxes, and class IDs
     """
+    model = get_model(model_version)
     results = model.predict(source=image, conf=conf, save=False)
     annotated_image = results[0].plot()  # annotated numpy image
     boxes = results[0].boxes.xyxy        # bounding box coordinates
